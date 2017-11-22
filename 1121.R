f83cd0c9e44533be658eb1706ada12e438bb2f8e
a511260195 <511260195@qq.com>
2017-11-21 21:13:35 -0800

    join

--- 1121.R
+++ 1121.R
@@ -1,1 +1,16 @@
-code
+library(plyr)
+library(dplyr)
+library(ggplot2)
+
+# read table
+coupon = read.csv("coupon.csv")
+data = read.csv("LocalArt.csv")
+
+# join table
+coupon$Coupon = "yes"
+data = join(data, coupon[,-1], by = "CustID" )
+data$Coupon = ifelse(is.na(data$Coupon),"no","yes")
+#  no(3983)  yes(1017) 
+
+
+
