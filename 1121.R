ee9d6b72dbfe39d407f021c77f92f3d501b9dcb9
zhuyuxuanjoe <zhuyuxuanjoe@gmail.com>
2017-11-21 21:52:53 -0800

    update kmeans clustering k = 4

--- 1121.R
+++ 1121.R
@@ -12,5 +12,16 @@ data = join(data, coupon[,-1], by = "CustID" )
 data$Coupon = ifelse(is.na(data$Coupon),"no","yes")
 #  no(3983)  yes(1017) 
 
+# merge
+data <- merge(data,coupon[,-1],all.x = T, by = "CustID")
+data$Coupon = ifelse(is.na(data$Coupon),"no","yes")
+data.feature <- data[,c(6:10)]
 
+#kmeans clustering
+library(useful)
+k.best <- FitKMeans(data.feature, max.clusters = 10,nstart = 25,seed = 100)
+plot(k.best) ## 4 clusters
 
+set.seed(1)
+k.out <- kmeans(data.feature,4)
+k.out
